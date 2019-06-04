c0cdb4b2ae96defce80c701d295a4c5354941b70
Masayuki Goto <originals0711@gmail.com>
2019-06-04 11:34:48 +0900

    add QR scan sample

---
+++ pwa-sample/qr-scan.js
@@ -0,0 +1,15 @@
+function openQRCamera(node) {
+  var reader = new FileReader();
+  reader.onload = function() {
+    node.value = "";
+    qrcode.callback = function(res) {
+      if(res instanceof Error) {
+        alert("QR コード画像が見つかりません。QR コードを含めた画像を指定してください。");
+      } else {
+        node.parentNode.previousElementSibling.value = res;
+      }
+    };
+    qrcode.decode(reader.result);
+  };
+  reader.readAsDataURL(node.files[0]);
+}
